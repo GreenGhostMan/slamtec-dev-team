9ca88bb4a2a6ee8ff078e7387c5e02ca6c5adefe
root <pyaesoanaung>
2021-10-08 09:16:13 +0000

    motor_update

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -3,40 +3,59 @@
 #include <rpos/features/motion_planner/feature.h>
 #include <rpos/core/pose.h>
 #include <chrono>
+#include <rpos/core/geometry.h>
 
-using namespace std;
+
 using namespace rpos::robot_platforms;
 
 int main(int argc,const char **argv){
 	SlamwareCorePlatform platform = SlamwareCorePlatform::connect(argv[1], 1445);
-	rpos::features::motion_planner::MoveOptionFlag MoveOptionFlagWithYaw ;
+	//rpos::features::motion_planner::MoveOptionFlag MoveOptionFlagWithYaw ;
 
-	std::vector<rpos::core::Location> pointsToGo;
-	pointsToGo.push_back(rpos::core::Location(0.5,0));
-
+	 std::vector<rpos::core::Location> pointsToGo;
+     pointsToGo.push_back(rpos::core::Location(2,0));
+	 //rpos::core::Location start(0.02,0);
+    
 	rpos::actions::MoveAction moveAction = platform.getCurrentAction();
-
+    //rpos::core::ACTION_DIRECTION actionDirection = rpos::core::ACTION_DIRECTION::FORWARD;
+	//rpos::core::Direction direction(actionDirection);
 	if(moveAction.getStatus() == rpos::core::ActionStatusFinished){
+        
+		//rpos::actions::MoveAction moveBy = platform.moveBy(direction);
+		//moveAction = platform.moveBy(start, false, true);
 		moveAction = platform.moveTo(pointsToGo, false, true);
+		
+		//moveAction = platform.moveTo(pointsToGo, false, true);
+		//moveAction = platform.moveTo(pointsToGo, false, true);
 
 		while(!(moveAction.getStatus()== rpos::core::ActionStatusFinished)){
 			if(moveAction.getStatus() == rpos::core::ActionStatusWaitingForStart){
-				cout<<"WAITING TO START"<<endl;
+				std::cout<<"WAITING TO START"<<std::endl;
 			}
 			else if(moveAction.getStatus() == rpos::core::ActionStatusRunning){
-				cout<<"RUNNING"<<endl;
+				rpos::core::Location location = platform.getLocation();
+                std::cout << "Robot Location: " << std::endl;
+                std::cout << "x: " << location.x() << ", ";
+                std::cout << "y: " << location.y() << std::endl;
+
+                rpos::core::Pose pose = platform.getPose();
+                std::cout << "Robot Pose: " << std::endl;
+                std::cout << "x: " << pose.x() << ", ";
+                std::cout << "y: " << pose.y() << ", ";
+                std::cout << "yaw: " << pose.yaw() << std::endl;
+				std::cout<<"RUNNING"<<std::endl;
 			}
 			else if(moveAction.getStatus() == rpos::core::ActionStatusPaused){
-				cout<<"PAUSED"<<endl;
+				std::cout<<"PAUSED"<<std::endl;
 			}
 			else if(moveAction.getStatus() == rpos::core::ActionStatusStopped){
-				cout<<"STOPPED"<<endl;
+				std::cout<<"STOPPED"<<std::endl;
 			}
 			else if(moveAction.getStatus() == rpos::core::ActionStatusError){
-				cout<<"ERROR"<<endl;
+				std::cout<<"ERROR"<<std::endl;
 			}		
 		}
-		cout<<"OK Finished"<<endl;
+		std::cout<<"OK Finished"<<std::endl;
 	}
 
 	return 0;
