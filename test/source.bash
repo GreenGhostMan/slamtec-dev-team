19f96e9d70b9205ba69e6f008365c0bf6ab59652
root <pyaesoanaung>
2021-10-21 08:39:34 +0000

    code add

--- test/source.bash
+++ test/source.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-runapp="/root/slamware_sdk_linux-x86_64-gcc4.8/linux-x86_64-release/output/"
+runapp="/slamware/slamware_sdk_linux-x86_64-gcc4.8/linux-x86_64-release/output/"
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -42,17 +42,45 @@ int main(int argc, const char* argv[]) {
 		rpos::robot_platforms::SlamwareCorePlatform sdp = rpos::robot_platforms::SlamwareCorePlatform::connect(argv[1],1445);
 		std::cout<<"SDK Version"<<sdp.getSDKVersion()<<std::endl;
 		std::cout<<"SDP Version"<<sdp.getSDPVersion()<<std::endl;
+
+		rpos::core::Pose pose = sdp.getPose();
+		std::cout << "Robot Before Pose: " << std::endl;
+		std::cout << "x: " << pose.x() << ", ";
+		std::cout << "y: " << pose.y() << ", ";
+		std::cout << "yaw: " << pose.yaw() << std::endl;
+
+		rpos::core::Location location1 = sdp.getLocation();
+                std::cout << "Robot Location: " << std::endl;
+                std::cout << "x: " << location1.x() << ", ";
+                std::cout << "y: " << location1.y() << std::endl;
+
 		rpos::actions::MoveAction action = sdp.getCurrentAction();
 		if(action)
 			action.cancel();
-		rpos::features::motion_planner::MoveOptions options;
-		options.flag = rpos::features::motion_planner::MoveOptionFlag( rpos::features::motion_planner::MoveOptionFlagMilestone |  rpos::features::motion_planner::MoveOptionFlagPrecise);
+		 rpos::features::motion_planner::MoveOptions options;
+		 options.flag = rpos::features::motion_planner::MoveOptionFlag( rpos::features::motion_planner::MoveOptionFlagMilestone |  rpos::features::motion_planner::MoveOptionFlagPrecise);
 		std::cout<<"Ready!!! "<<std::endl;
-		// action = sdp.moveTo(rpos::core::Location(2,0),options); // a loot g twr ml :3
-		action = sdp.rotateTo(rpos::Rotation(pi,0,0))
+		action = sdp.moveTo(rpos::core::Location(-0.3,0),options,0); // a loot g twr ml :3
+		// action = sdp.rotateTo(rpos::core::Rotation(pi,0,0));
+		
+
+		//action = sdp.rotateTo(rpos::core::Rotation(0,0,0));
+
+
 		std::cout<<" Rotating "<<std::endl;
 		action.waitUntilDone();
 		std::cout<<" Finished "<<std::endl;
+		rpos::core::Pose pose2 = sdp.getPose();
+		std::cout << "Robot after Pose: " << std::endl;
+		std::cout << "x: " << pose2.x() << ", ";
+		std::cout << "y: " << pose2.y() << ", ";
+		std::cout << "yaw: " << pose2.yaw() << std::endl;
+
+		rpos::core::Location location2= sdp.getLocation();
+        std::cout << "Robot Location: " << std::endl;
+            std::cout << "x: " << location2.x() << ", ";
+                std::cout << "y: " << location2.y() << std::endl;
+
 		if(action.getStatus()==rpos::core::ActionStatusError)
 			std::cout<<"Action Failed: "<<action.getReason()<<std::endl;
 		// //draw a virtual track from (0, 0) to (2, 0), then move to (0, 0) via virtual track
